8f33fc901c05f397e3d3471e3d378254d39df1ce
Sasi Kumar M <sasikumarmurugan02@gmail.com>
2023-02-28 17:08:32 +0530

    28.02.23

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
+//import ExpenseItem from './Component/Expenses/ExpenseItem/ExpenseItem'; // ExpenseItem is a component which is holded by ExpenseItem.jsx
+import Expenses from './Component/Expenses/Expenses';
+import NewExpense from './Component/New_expense/NewExpense';
+import Counter from './Counter';
+const DUMMY_DATA = [
+  {
+    id: 'e1',
+    title: 'Groceries',
+    amount: 94.12,
+    date: new Date(2021, 7, 14),
+  },
+  { id: 'e2', title: 'New Mobile', amount: 799.49, date: new Date(2020, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Repair',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New work desk',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 function App() {
+  const [expenses, setExpenses] = useState(DUMMY_DATA);
+  const onAddExpense = (newValue) => {
+    setExpenses((prev)=>[newValue, ...prev]);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <Counter/>
+    <div className='App'>
+      <NewExpense onAddExpense={onAddExpense}/>
+      {<Expenses expenses = {expenses}/>}
+      
     </div>
   );
 }
